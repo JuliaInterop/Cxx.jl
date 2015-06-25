63b130b3a2caca1f5bc8685c469f6dd16a5aa1a3
Keno Fischer <kfischer@college.harvard.edu>
2015-06-25 17:12:13 -0400

    Add a basic list autowrap macro

--- src/autowrap.jl
+++ src/autowrap.jl
@@ -3,3 +3,25 @@ using Base.Meta
 for binop in keys(cxx_binops)
     @eval Base.($(quot(binop)))(x::Union(CppValue,CppRef),y::Union(CppValue,CppRef)) = @cxx ($binop)(x,y)
 end
+
+macro list(t)
+    q = quote
+        Base.start(it::$t) = 0
+        Base.next(it::$t,i) = (it[i], i+1)
+        Base.done(it::$t,i) = i >= length(it)
+    end
+    if isexpr(t,:macrocall)
+        if t.args[1] == symbol("@pcpp_str")
+            append!(q.args,(quote
+                Base.getindex(it::$t,i) = icxx"(*$(it))[$i];"
+                Base.length(it::$t) = icxx"$(it)->size();"
+            end).args)
+        elseif t.args[1] == symbol("@vcpp_str")
+            append!(q.args,(quote
+                Base.getindex(it::$t,i) = icxx"$(it)[$i];"
+                Base.length(it::$t) = icxx"$(it).size();"
+            end).args)
+        end
+    end
+    esc(q)
+end
