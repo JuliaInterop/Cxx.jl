c7eca5f1d544bfc9f15ee6ed12af5ce7b4083c22
Gnimuc <qiyupei@gmail.com>
2021-08-15 19:41:16 +0900

    Remove `boot` folder

--- boot/boot.cpp
+++
@@ -1,9 +0,0 @@
-#include "boot.h"
-#include <iostream>
-
-// this function is for sanity check only
-void print_julia_version(void) {
-  const char *ptr = jl_ver_string();
-  std::string s(ptr);
-  std::cout << s << std::endl;
-}
